60d1ae91ebd274f7bc61c379f59d04a915a4ccc8
Cy Dalby <cda01@sbgml02346.sbg.skybettingandgaming.com>
2018-09-20 13:33:02 +0100

    Wednesday's work

--- public/burger.js
+++ public/burger.js
@@ -1,11 +1,11 @@
 $('.burgerMenu').on('click', function(ev){
 	ev.preventDefault();
 	$(this).toggleClass('animateBurger');
-	$('nav').slideToggle('fast');
+	$('nav').slideToggle('slow');
 });
 $(window).on('resize', function(ev){
 	//console.info(window.innerWidth);
-	if(window.innerWidth > 600){
+	if(window.innerWidth > 900){
 		$('nav ul').attr('style','');	
 	};
 });
