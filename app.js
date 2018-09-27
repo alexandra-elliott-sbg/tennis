d3940a54376d49425f9c5cdc55b739abd04c19ae
Cy Dalby <cda01@sbgml02346.sbg.skybettingandgaming.com>
2018-09-27 14:33:00 +0100

    Cy work

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-
-var fs = require('fs')
+var fs = require('fs');
 var path = require('path');
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 var routes = require('./routes/routes');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-app.use(express.static('./public'))
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('./public'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,10 +16,9 @@ routes(app)
 
 var MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (err, client) => {
+MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, function (err, client) {
 
-  app.set('myDb', client.db('staffDb'));
-
+  app.set('myDb', client.db('tennis'));
 
 })
 
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,7 @@
 module.exports = {
 
     viewAll: (app, req, res) => {
-        // res.send("View All");
-        app.get('myDb').collection('staffCollection').find({}).toArray((err, docs) => {
+        app.get('myDb').collection('bets').find({}).toArray((err, docs) => {
 
             if (err) {
                 console.error(err)
@@ -11,61 +10,20 @@ module.exports = {
         })
     },
 
-    viewSingle: (app, req, res) => {
-        let staffID = req.params.staffID;
-        staffID = parseInt(staffID);
-        //res.send(`Just one record looking for ${staffID}`);
-        app.get('myDb').collection('staffCollection').find({'staffID': staffID}).toArray((err, docs) => {
+    addBet: (app, req, res) => {
+        let newBet = req.body;
+        console.log(newBet);
 
+        // assign bet ID
+        
+      
+       
+        app.get('myDb').collection('bets').insertOne(newBet, (err, docs) => {
             if (err) {
                 console.error(err)
             }
-            res.json(docs)
+            console.log(docs);
+            res.json({ 'msg': 'successful' })
         })
-    },
-
-    addStaff: (app, req, res) => {
-        let newStaff = req.body;
-        let staffID = parseInt(newStaff.staffID);
-        newStaff.staffID = staffID
-        console.info(newStaff);
-        // res.send('post')
-        // res.send(`here we'll add a record with this data `);
-        app.get('myDb').collection('staffCollection').insertOne(newStaff, (err, docs) => {
-            if (err) {
-                console.error(err)
-            }
-            res.json({'msg':'successful'})
-        })
-    },
-    editStaff: (app, req, res) => {
-        var amendStaff = req.body;
-        var staffID = parseInt(amendStaff.staffID);
-        app.get('myDb').collection('staffCollection').updateOne(
-            {'staffID': staffID},
-            { $set: {
-                'staffName' : amendStaff.staffName,
-                'staffEmail': amendStaff.staffEmail}
-            },
-            (err, docs) => {
-            if (err) {
-                console.error(err)
-            }
-            res.json({'msg':'successful'})
-        })
-    },
-    deleteStaff: (app, req, res) => {
-        var deleteStaff = req.body;
-        var staffID = parseInt(deleteStaff.staffID);
-        app.get('myDb').collection('staffCollection').deleteOne({'staffID': staffID}, (err, dbResp) => {
-            if (err) {
-                console.error(err)
-            }
-            if(dbResp.deletedCount === 1){
-                res.json({'msg':'successful'})
-            }else{
-                res.json({'msg':'not found'}) 
-            }
-           
-        })
-    }}
+    }
+}
